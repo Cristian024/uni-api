09f1aa7840286bd418c1c00d0c72921e7ddf9f59
CristianRobledo0705 <cristiandavidrobledoleo@gmail.com>
2024-06-20 12:57:59 -0500

    fix(Update index.php): cors error fixed

--- api/index.php
+++ api/index.php
@@ -13,6 +13,11 @@ ServerController::validateCorrectPetition();
 parse_str($_SERVER['QUERY_STRING'], $params);
 $method = $_SERVER['REQUEST_METHOD'];
 
+header("Content-type: application/json; charset=utf-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
 $path = isset ($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
 $idExplode = explode('/', $path);
 $queryId = ($path !== '/') ? end($idExplode) : null;
