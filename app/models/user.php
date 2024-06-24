fcb3b18a1dc8f7e3f41b33445efdc145e64d18cf
CristianRobledo0705 <cristiandavidrobledoleo@gmail.com>
2024-06-23 23:32:16 -0500

    refactor(Update user.php): add new params to user entity

--- app/models/user.php
+++ app/models/user.php
@@ -12,17 +12,31 @@ class User
 {
     public $id;
     public $name;
+    public $last_name;
     public $age;
     public $email;
     public $password;
+    public $cellphone;
+    public $date_of_birth;
+    public $gender;
+    public $faculty;
+    public $career;
+    public $student_code;
 
-    function __construct($id, $name, $age, $email, $password)
+    function __construct($id, $name, $last_name, $age, $email, $password, $cellphone, $date_of_birth, $gender, $faculty, $career, $student_code)
     {
         $this->id = $id;
         $this->name = $name;
+        $this->last_name = $last_name;
         $this->age = $age;
         $this->email = $email;
         $this->password = $password;
+        $this->cellphone = $cellphone;
+        $this->date_of_birth = $date_of_birth;
+        $this->gender = $gender;
+        $this->faculty = $faculty;
+        $this->career = $career;
+        $this->student_code = $student_code;
     }
 
     public static function getUsers($field)
@@ -58,6 +72,9 @@ class User
         if (!isset($params['name']))
             ResponseController::sentBadRequestResponse('Name not provided');
 
+        if (!isset($params['last_name']))
+            ResponseController::sentBadRequestResponse('Last name not provided');
+
         if (!isset($params['email']))
             ResponseController::sentBadRequestResponse('Email not provided');
 
@@ -66,7 +83,7 @@ class User
         if ($userExists != null) {
             ResponseController::sentBadRequestResponse('User already exists');
         } else {
-            $user = new User(null, $params['name'], null, $params['email'], md5($params['password']));
+            $user = new User(null, $params['name'], $params['last_name'], null, $params['email'], md5($params['password']), null, null, null, null, null, null);
             $fields = DatabaseHelper::extractParams(User::class, $user, 'insert');
             $response = User::insertUser($fields);
 
