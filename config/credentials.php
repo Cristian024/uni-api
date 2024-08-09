b1b9f0c8dfc1f98ccb4f5c010ab5dd5abf7296be
Cristian David Robledo León <110924565+Cristian024@users.noreply.github.com>
2024-08-09 10:57:36 -0500

    Update credentials.php

--- config/credentials.php
+++ config/credentials.php
@@ -6,17 +6,6 @@ use App\Controllers\General\ResponseController;
 
 class Credentials
 {
-    /*
-    ####PRODUCTION####
-    public static $SERVER = "roundhouse.proxy.rlwy.net";
-    public static $USER = "root";
-    public static $PASSWORD = "mlHCNqynAOKwbMsRfsuWBevsHwXvesri";
-    public static $DATABASE = "railway";
-    public static $PORT = "33345";
-    public static $ACCESS_TOKEN = "Fq0830jA9h5pEeAvdTW5wDglb9JFqBju5RDtls5xKGVVXJAPOwto3bB5ivvVU14E";
-    */
-
-    ####DEV####
     public static $SERVER = "";
     public static $USER = "";
     public static $PASSWORD = "";
